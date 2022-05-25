fb49593c799457a5d3a08557e757f898c0c8cdd0
Eduardo Teixeira <dunirvana@gmail.com>
2022-05-25 15:01:37 -0300

    Inclusão de validações no modelo com DataAnnotations.

--- Filmes.API/Models/Filme.cs
+++ Filmes.API/Models/Filme.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +8,19 @@ namespace Filmes.API.Models
 {
     public class Filme
     {
+        [Required]
+        [StringLength(200)]
         public string Titulo { get; set; }
 
+        [StringLength(60)]
         public string Genero{ get; set; }
 
+        [Required]
+        [StringLength(100)]
         public string Diretor { get; set; }
 
+        [Required]
+        [Range(1, 600)]
         public int Duracao { get; set; }
 
         public override string ToString()
