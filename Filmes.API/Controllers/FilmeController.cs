6d9ccbfe8f3cc55ee0226b44b82dbbb01a19e999
Eduardo Teixeira <dunirvana@gmail.com>
2022-05-25 13:06:10 -0300

    Inclusão de filmes (inicio).

---
+++ Filmes.API/Controllers/FilmeController.cs
@@ -0,0 +1,23 @@
+﻿using Filmes.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Filmes.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionarFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine($"\r\n[AdicionarFilme]\r\n{filme}");
+        }
+    }
+}
---
+++ Filmes.API/Models/Filme.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Filmes.API.Models
+{
+    public class Filme
+    {
+        public string Titulo { get; set; }
+
+        public string Genero{ get; set; }
+
+        public string Diretor { get; set; }
+
+        public int Duracao { get; set; }
+
+        public override string ToString()
+        {
+            return $"Filme:{Titulo} - {Genero}\r\nDiretor:{Diretor} Duracao:{Duracao}";
+        }
+    }
+}
